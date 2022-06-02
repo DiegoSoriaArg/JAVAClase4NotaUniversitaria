22995bfb45d95b13dd8ee9522f2e7440dd6080bb
DiegoSoriaArg <diegosoriaarg@gmail.com>
2022-06-02 17:05:08 -0300

    Subiendo el proyecto NotaUni

---
+++ src/nota/Nota.java
@@ -0,0 +1,40 @@
+package nota;
+
+public class Nota {
+	
+	private int valorNota;
+
+	public Nota(int valorInicial) {
+		this.setValorNota(valorInicial);
+	}
+	
+	public int obtenerValor() {
+		return this.valorNota;
+	}
+	
+	public boolean aprobado() {
+		return (this.valorNota >= 4);
+	}
+	
+	public boolean desaprobado() {
+		return (this.valorNota < 4);
+	}
+	
+	public void recuperar(int nuevoValor) {
+		if (nuevoValor > this.valorNota)
+			this.valorNota = nuevoValor;
+	}
+	
+	private boolean esValorCorrecto(int x) {
+		return (x >= 0 && x <= 10);
+	}
+	
+	private void setValorNota(int nota) {
+		if (!esValorCorrecto(nota)) {
+			Error notaInvalida = new Error("Nota invalidad ingresada");
+			throw notaInvalida;	
+		} 
+		this.valorNota = nota;
+	}
+
+}
